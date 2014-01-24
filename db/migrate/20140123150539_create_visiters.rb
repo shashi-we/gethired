dc2d49a1fb87ab177249c018dd708491269543d7
surendra <shashi.yuvasoft@gmail.com>
2014-01-25 04:39:21 +0530

    changes for spelling mistake visitores

--- db/migrate/20140123150539_create_visiters.rb
+++ db/migrate/20140123150539_create_visiters.rb
@@ -1,11 +1,14 @@
 class CreateVisiters < ActiveRecord::Migration
-  def change
-    create_table :visiters do |t|
+  def self.up
+    create_table :visitors do |t|
       t.string  :email_id
       t.string  :template_name
       t.integer :price
 
       t.timestamps
     end
+  end
+  def self.down
+  	drop_table :visitors
   end
 end
