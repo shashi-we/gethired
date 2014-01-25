b5967525168e87f83f507665c9bd5af91bc63710
surendra <shashi.yuvasoft@gmail.com>
2014-01-25 17:20:45 +0530

    refs to changes for orders status

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,6 +6,48 @@ class Order < ActiveRecord::Base
     :path => "/orders/:attachment/:id/:style.:extension",
     :s3_credentials => "#{Rails.root}/config/s3.yml",
     :s3_protocol => 'https'
-  #validates_attachment :document, :presence => true
+  
+  #admin settings
 
+ 	# def roles_enum
+    
+ 	# end
+
+  rails_admin do
+    edit do
+    	field :template_name do 
+      
+      end
+      field :template_price do 
+      
+      end
+      field :complete_day do 
+      
+      end
+      field :complete_day_price do 
+      
+      end
+      field :number_of_page do 
+      
+      end
+      field :page_price do 
+      
+      end
+      field :color do 
+      
+      end
+      field :color_price do 
+      
+      end
+      field :user_id do 
+      
+      end
+      field :status,:enum do 
+        enum do
+			    [ [ 'Processing', 'Processing' ], [ 'Designing', 'Designing' ], [ 'Revisions', 'Revisions' ], [ 'Completed', 'Completed' ]]
+			  end
+      end
+    end
+  end
+  
 end
